d47af64fdfa9bce10a14d994330aced153d083a9
m2990 <m2990@m2990s-iMac.local>
2020-09-03 17:48:07 -0500

    Added create and show methods to controller

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -2,4 +2,18 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :body))
+ 
+        @article.save
+        redirect_to @article
+    end
 end
--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/articles', controller: 'articles', action: 'index'
+  #get '/articles', controller: 'articles', action: 'index'
+  resources :articles
+  root to: "articles#index"
+  get '/articles', to: 'articles#index'
+  get '/articles/:id(.:format)', to: 'articles#show'
+  get 'articles/new', to: 'articles#new'
+  post '/articles/new', to: 'articles#create'
+  get '/articles/:id/edit', to: 'articles#edit'
+  put '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destory'
 end
